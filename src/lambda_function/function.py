d34dad2ac9b2699cb455c8a46801200dea63a71c
Jishna Suresh <jsuresh@quinovas.com>
2019-03-27 18:08:57 +0530

    Initial commit

---
+++ src/lambda_function/function.py
@@ -0,0 +1,15 @@
+import json	
+import logging.config
+import os
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def handler(event, context):
+	logger.info('Event :{}'.format(event))
+	return event
+
+		
+
