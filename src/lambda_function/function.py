f841ec14ce5a87bc4e40771ee9c266e118a0dc41
Joseph Wortmann <joseph.wortmann@gmail.com>
2019-04-04 13:57:44 -0500

    updated to correct logging and exceptions

--- src/lambda_function/function.py
+++ src/lambda_function/function.py
@@ -1,4 +1,5 @@
 import boto3
+import json
 import logging.config
 import os
 
@@ -13,7 +14,7 @@ logger.setLevel(logging.INFO)
 
 
 def handler(event, context):
-	logger.info('Event :{}'.format(event))
+	logger.info('Processing event {}'.format(json.dumps(event)))
 	with _connect(event).cursor() as cursor:
 		return _process_results(
 			cursor.execute(event['Operation'], event.get('Parameters')), 
@@ -34,10 +35,12 @@ def _connect(event):
 def _process_results(cursor, single_result):
 	results = []
 	description = cursor.description
+	logger.debug('Query "{}" returned in {}ms'.format(cursor.query, cursor.execution_time_in_millis))
 	for row in cursor:
 		if single_result and len(results) == 1:
-			logger.info('Expected single row but returned more than one row in results for the query : {}'.format(cursor.query))
+			raise Exception('Expected single row but returned more than one row in results for the query : {}'.format(cursor.query))
 		results.append(_map_result(description, row))
+	logger.debug('Query returned {} results'.format(len(results)))
 	return results if not single_result else results[0] if results else {}
 	
 def _map_result(description, row):
